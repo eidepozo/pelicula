ac3e17d973da483e0b39f6e1e1dd62654801569f
eidepozo <eidepozo@gmail.com>
2022-01-27 11:49:06 -0300

    Agrega configuracion provider de pelciulas

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:pelicula/providers/movies_provider.dart';
 import 'package:pelicula/screens/screens.dart';
 import 'package:pelicula/themes/app_themes.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (_) => MoviesProvider(), lazy: false)
+    ], child: MyApp());
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
